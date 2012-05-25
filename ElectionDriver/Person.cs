fe9e5817e4e86023e4231e850cc09657773a39fd
Gordon Watts <gordon@gordonandpaula.net>
2012-05-25 15:02:24 -0400

    First versions of the person object

---
+++ ElectionDriver/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionDriver
+{
+    /// <summary>
+    /// Holds onto voting preferences for a single person
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Keep track of the candidate ordering. Position i contains the ranking for candidate i.
+        /// </summary>
+        private int[] _candidateOrdering;
+
+        /// <summary>
+        /// Create Person and stash the random voting preferences for the number
+        /// of given candidates.
+        /// </summary>
+        /// <param name="candidates"></param>
+        public Person(int candidates, Random generator)
+        {
+            var ordering = from i in Enumerable.Range(0, candidates)
+                           select new
+                           {
+                               Index = i,
+                               Prob = generator.Next()
+                           };
+
+            _candidateOrdering = (from possible in ordering
+                                 orderby possible.Prob
+                                 select possible.Index).ToArray();
+        }
+
+        /// <summary>
+        /// Get the # of candidates in this person.
+        /// </summary>
+        public int NumberOfCandidates
+        {
+            get { return _candidateOrdering.Length; }
+        }
+    }
+}
---
+++ t_ElectionDriver/t_Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using ElectionDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_ElectionDriver
+{
+    [TestClass]
+    public class t_Person
+    {
+        [TestMethod]
+        public void TestCreation()
+        {
+            var p = new Person(5, new Random());
+            Assert.AreEqual(5, p.NumberOfCandidates, "# of candidates");
+        }
+
+        [TestMethod]
+        public void TestForFlat()
+        {
+            int nCandidates = 5;
+            var people = from i in Enumerable.Range(0, 1000)
+                         select new Person(nCandidates, new Random());
+
+            var rankings = from p in people
+                           from candidate in Enumerable.Range(0, nCandidates)
+                           select 1;
+        }
+    }
+}
