e01a39a2b3c20aea8f8a3b5bcfec5701d13bcaab
Gordon Watts <gordon@gordonandpaula.net>
2012-05-25 23:20:51 -0400

    Two classes... I think I need.

---
+++ ElectionDriver/Election.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionDriver
+{
+    /// <summary>
+    /// Run an election given a list of people who already know about and have ranked
+    /// the candidates. It also will re-run the election a few times and see how the
+    /// winner changes as the number of candidates drops out.
+    /// </summary>
+    public class Election
+    {
+    }
+}
