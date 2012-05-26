c84b1a6f0b316dccce40a465ecd9ac56e7f23c4c
Gordon Watts <gordon@gordonandpaula.net>
2012-05-26 00:54:28 -0400

    Run the election dropping one person out and look for flips
    This is the basic code we want to run in the end.

--- RunMajorityElection/Program.cs
+++ RunMajorityElection/Program.cs
@@ -15,14 +15,9 @@ namespace RunMajorityElection
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int nPeople = 2000;
+            int nPeople = 4000;
+            int nElections = 50;
             int nCandidates = 3;
-
-            // A bunch of people
-
-            var r = new Random();
-            var people = (from idx in Enumerable.Range(0, nPeople)
-                         select new Person(nCandidates, r)).ToArray();
 
             // The scoring system for simple majority is just who is out front.
             // The score function just returns a list of numbers for each person.
@@ -49,9 +44,59 @@ namespace RunMajorityElection
                     return candidateWeights;
                 };
 
+            // A bunch of people
+
+            int flips = 0;
+
+            for (int i = 0; i < nElections; i++)
+            {
+                Console.WriteLine();
+                int localFlips = RunElection(nPeople, nCandidates, pScore, eScore);
+                if (localFlips != 0)
+                    flips += 1;
+            }
+
+            Console.WriteLine("Number of flips was {0} in {1} elections.", flips, nElections);
+        }
+
+        private static int RunElection(int nPeople, int nCandidates, Func<IEnumerable<CandiateRanking>, IEnumerable<CandiateRanking>> pScore, Func<IEnumerable<IEnumerable<CandiateRanking>>, IEnumerable<CandiateRanking>> eScore)
+        {
+            var r = new Random();
+            var people = (from idx in Enumerable.Range(0, nPeople)
+                          select new Person(nCandidates, r)).ToArray();
+
+            // The election
+
+            var winner = RunElection(people, pScore, eScore, printResult: true);
+
+            // The result
+
+            int flips = 0;
+            for (int i = 0; i < nCandidates; i++)
+            {
+                if (i != winner)
+                {
+                    var newwinner = RunElection(people, pScore, eScore, new int[] { i });
+                    if (newwinner != winner)
+                        flips++;
+                }
+            }
+
+            return flips;
+        }
+
+        /// <summary>
+        /// Runs one election
+        /// </summary>
+        /// <param name="people"></param>
+        /// <param name="pScore"></param>
+        /// <param name="eScore"></param>
+        /// <returns>Candidate who won.</returns>
+        private static int RunElection(Person[] people, Func<IEnumerable<CandiateRanking>, IEnumerable<CandiateRanking>> pScore, Func<IEnumerable<IEnumerable<CandiateRanking>>, IEnumerable<CandiateRanking>> eScore, int[] candidatesToDrop = null, bool printResult = false)
+        {
             // Score the people.
             var peopleScored = from p in people
-                               select pScore(p.FullRanking());
+                               select pScore(p.FullRanking(candidatesToDrop));
 
             // Now score the election
             var election = eScore(peopleScored);
@@ -59,8 +104,31 @@ namespace RunMajorityElection
             var winner = (from er in election
                           orderby er.ranking descending
                           select er).First();
-            Console.WriteLine("Winner of the election is candidate {0} with {1} votes.", winner.candidate, winner.ranking);
 
+            var bld = new StringBuilder();
+            bld.AppendFormat("Winner of the election is candidate {0} with {1} votes", winner.candidate, winner.ranking);
+
+            if (printResult)
+            {
+                if (candidatesToDrop != null)
+                {
+                    bld.Append("(dropped candidates ");
+                    bool isfirst = true;
+                    foreach (var c in candidatesToDrop)
+                    {
+                        if (!isfirst)
+                            bld.Append(", ");
+                        isfirst = false;
+                        bld.Append(c);
+                    }
+                    bld.Append(")");
+                }
+                bld.Append(".");
+
+                Console.WriteLine(bld.ToString());
+            }
+
+            return winner.candidate;
         }
     }
 }
