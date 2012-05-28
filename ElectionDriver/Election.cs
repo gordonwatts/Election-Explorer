a3672f990048697dcb8e17a67fdc3db6fb54c929
Gordon Watts <gordon@gordonandpaula.net>
2012-05-27 23:17:20 -0700

    First version of the election code - runs only a single election so far.

--- ElectionDriver/Election.cs
+++ ElectionDriver/Election.cs
@@ -7,11 +7,72 @@ using System.Threading.Tasks;
 namespace ElectionDriver
 {
     /// <summary>
-    /// Run an election given a list of people who already know about and have ranked
-    /// the candidates. It also will re-run the election a few times and see how the
-    /// winner changes as the number of candidates drops out.
+    /// Told how many people in an election and a way to run it, run the election, and
+    /// analyze it by dropping each of the other candidates in turn.
     /// </summary>
     public class Election
     {
+        public Election()
+        {
+            NumberOfPeople = 400;
+            NumberOfCandidates = 1000;
+        }
+
+        /// <summary>
+        /// Get/Set the number of poeple that are going to vote.
+        /// </summary>
+        public int NumberOfPeople { get; set; }
+
+        /// <summary>
+        /// Get/Set the number of candidates that are going to run.
+        /// </summary>
+        public int NumberOfCandidates { get; set; }
+
+        /// <summary>
+        /// Run an election and return the results.
+        /// </summary>
+        public CandiateRanking[] RunSingleElection()
+        {
+            if (_steps.Count == 0)
+                throw new InvalidOperationException("Election has no steps defined");
+
+            // Generate the people
+            var people = GeneratePeople().ToArray();
+
+            CandiateRanking[] stepResult = null;
+            foreach (var s in _steps)
+            {
+                stepResult = s.RunStep(people);
+                if (stepResult.Length == 1)
+                    return stepResult;
+            }
+
+            return stepResult;
+        }
+
+        /// <summary>
+        /// Generate the people we want.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<Person> GeneratePeople()
+        {
+            var r = new Random();
+            return from i in Enumerable.Range(0, NumberOfPeople)
+                   select new Person(NumberOfCandidates, r);
+        }
+
+        /// <summary>
+        /// The list of steps that we go through for an election.
+        /// </summary>
+        List<IElectionStep> _steps = new List<IElectionStep>();
+
+        /// <summary>
+        /// Add a step to the election steps.
+        /// </summary>
+        /// <param name="step"></param>
+        public void AddStep(ESOnlyBestCounts step)
+        {
+            _steps.Add(step);
+        }
     }
 }
---
+++ t_ElectionDriver/t_Election.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ElectionDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_ElectionDriver
+{
+    [TestClass]
+    public class t_Election
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestBlankRun()
+        {
+            var e = new Election();
+            e.RunSingleElection();
+        }
+
+        [TestMethod]
+        public void TestSimpleRun()
+        {
+            var e = new Election();
+            e.AddStep(new ESOnlyBestCounts());
+            var r = e.RunSingleElection();
+        }
+    }
+}
