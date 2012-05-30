f419945e5c47e944d4143b74cacf06a694cd7a34
Gordon Watts <gordon@gordonandpaula.net>
2012-05-29 21:12:09 -0700

    A filtering election step - keeps only those better than some fraction of hte vote.

---
+++ ElectionDriver/ESKeepThoseBetterThan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionDriver
+{
+    /// <summary>
+    /// A filtering election step - keep only those candidates that have
+    /// more than some fraction of all the votes.
+    /// </summary>
+    public class ESKeepThoseBetterThan : IElectionStep
+    {
+        /// <summary>
+        /// Create an election step that will keep only the candidates who have a minFrac
+        /// of the votes.
+        /// </summary>
+        /// <param name="minFrac"></param>
+        public ESKeepThoseBetterThan(double minFrac)
+        {
+            MinimumFraction = minFrac;
+            DoNothingIfNoOnePasses = true;
+        }
+
+        /// <summary>
+        /// Window out the list of candidates.
+        /// </summary>
+        /// <param name="people"></param>
+        /// <param name="previousResults"></param>
+        /// <returns></returns>
+        public CandiateRanking[] RunStep(Person[] people, CandiateRanking[][] previousResults)
+        {
+            if (previousResults == null || previousResults.Length == 0)
+                throw new ArgumentException("Cant run the elections step that keeps the best guys if we don't have the result of an election!");
+
+            var lastStepResults = previousResults[previousResults.Length-1];
+            var passed = (from c in lastStepResults
+                          where ((double)c.ranking) / ((double)people.Length) > MinimumFraction
+                          select c).ToArray();
+
+            if (passed.Length == 0)
+            {
+                if (DoNothingIfNoOnePasses)
+                    return lastStepResults;
+                return new CandiateRanking[] { };
+            }
+
+            return passed;
+        }
+
+        /// <summary>
+        /// Get Set the minimum fraction to keep.
+        /// </summary>
+        public double MinimumFraction { get; set; }
+
+        /// <summary>
+        /// If set to true (default) then the candidate list is returned if there is no one above
+        /// the fraction.
+        /// </summary>
+        public bool DoNothingIfNoOnePasses { get; set; }
+    }
+}
---
+++ t_ElectionDriver/t_ESKeepThoseBetterThan.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ElectionDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_ElectionDriver
+{
+    [TestClass]
+    public class t_ESKeepThoseBetterThan
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNotFirstStep()
+        {
+            var people = new Person[] { new Person(0, 1, 2) };
+            var old = new CandiateRanking[][] { };
+
+            var s = new ESKeepThoseBetterThan(0.5);
+            s.RunStep(people, old);
+        }
+
+        [TestMethod]
+        public void TestSimpleRun()
+        {
+            var people = new Person[] {
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+            };
+            var old = new CandiateRanking[][] { new CandiateRanking[] { new CandiateRanking(0, 3), new CandiateRanking(1, 1) } };
+
+            var s = new ESKeepThoseBetterThan(0.7);
+            var r = s.RunStep(people, old);
+            Assert.AreEqual(1, r.Length, "# of candidates ranked");
+            Assert.AreEqual(0, r[0].candidate, "Incorrect candidate came back");
+        }
+
+        [TestMethod]
+        public void TestSimpleRunNoOneDefault()
+        {
+            var people = new Person[] {
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+            };
+            var old = new CandiateRanking[][] { new CandiateRanking[] { new CandiateRanking(0, 2), new CandiateRanking(1, 2) } };
+
+            var s = new ESKeepThoseBetterThan(0.7);
+            var r = s.RunStep(people, old);
+            Assert.AreEqual(2, r.Length, "# of candidates ranked");
+            Assert.AreEqual(0, r[0].candidate, "Incorrect candidate came back");
+            Assert.AreEqual(1, r[1].candidate, "Incorrect candidate came back");
+        }
+
+        [TestMethod]
+        public void TestSimpleRunNoOneFailWhen()
+        {
+            var people = new Person[] {
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+                new Person(0, 1, 2),
+            };
+            var old = new CandiateRanking[][] { new CandiateRanking[] { new CandiateRanking(0, 2), new CandiateRanking(1, 2) } };
+
+            var s = new ESKeepThoseBetterThan(0.7) { DoNothingIfNoOnePasses=false };
+            var r = s.RunStep(people, old);
+            Assert.AreEqual(0, r.Length, "# of candidates ranked");
+        }
+    }
+}
