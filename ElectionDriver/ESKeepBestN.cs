39fd180e7a4215be6d9bd422d17374f890fd0d70
Gordon Watts <gordon@gordonandpaula.net>
2012-05-30 22:27:56 -0700

    Added election step that will keep the best candidate of N in the list.

---
+++ ElectionDriver/ESKeepBestN.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionDriver
+{
+    /// <summary>
+    /// Election step to keep the top n candidates. If there are less, keep them all.
+    /// </summary>
+    public class ESKeepBestN : IElectionStep
+    {
+        /// <summary>
+        /// Init with the number of candidates to keep
+        /// </summary>
+        /// <param name="candidatesToKeep"></param>
+        public ESKeepBestN(uint candidatesToKeep)
+        {
+            if (candidatesToKeep == 0)
+                throw new ArgumentException("Can't create a Keep Best N election step where we keep zero candidates!");
+            CandidatesToKeep = candidatesToKeep;
+        }
+
+        /// <summary>
+        /// Run the election
+        /// </summary>
+        /// <param name="people"></param>
+        /// <param name="previousResults"></param>
+        /// <returns></returns>
+        public CandiateRanking[] RunStep(Person[] people, CandiateRanking[][] previousResults)
+        {
+            if (previousResults == null || previousResults.Length == 0)
+                throw new InvalidOperationException("Can't run a Keep Best N candidates election step as first step in an election!");
+
+            return (from r in previousResults.Last()
+                    orderby r.ranking descending
+                    select r).Take((int)CandidatesToKeep).ToArray();
+        }
+
+        /// <summary>
+        /// Get the # of candidates to keep
+        /// </summary>
+        public uint CandidatesToKeep { get; private set; }
+    }
+}
---
+++ t_ElectionDriver/t_ESKeepBestN.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using ElectionDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace t_ElectionDriver
+{
+    /// <summary>
+    /// Test the keep best of guy
+    /// </summary>
+    [TestClass]
+    public class t_ESKeepBestN
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestKeepZero()
+        {
+            var es = new ESKeepBestN(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestKeepFirstInChain()
+        {
+            var es = new ESKeepBestN(10);
+            es.RunStep(new Person[] { new Person(0, 1, 2, 3) }, null);
+        }
+
+        [TestMethod]
+        public void TestKeep2of5()
+        {
+            var cr = new CandiateRanking[][] { new CandiateRanking[] {
+                new CandiateRanking(0, 10),
+                new CandiateRanking(1, 9),
+                new CandiateRanking(2, 8),
+                new CandiateRanking(3, 7),
+                new CandiateRanking(4, 6)
+            }};
+            var es = new ESKeepBestN(2);
+            var r = es.RunStep(new Person[] { new Person(0, 1, 2, 3) }, cr);
+            Assert.AreEqual(2, r.Length, "# of kept candidates");
+            Assert.AreEqual(0, r[0].candidate, "First candidate");
+            Assert.AreEqual(1, r[1].candidate, "Second candidate");
+        }
+
+        [TestMethod]
+        public void TestKeep2of5OutOfOrder()
+        {
+            var cr = new CandiateRanking[][] { new CandiateRanking[] {
+                new CandiateRanking(0, 6),
+                new CandiateRanking(1, 7),
+                new CandiateRanking(2, 8),
+                new CandiateRanking(3, 9),
+                new CandiateRanking(4, 10)
+            }};
+            var es = new ESKeepBestN(2);
+            var r = es.RunStep(new Person[] { new Person(0, 1, 2, 3) }, cr);
+            Assert.AreEqual(2, r.Length, "# of kept candidates");
+            Assert.AreEqual(4, r[0].candidate, "First candidate");
+            Assert.AreEqual(3, r[1].candidate, "Second candidate");
+        }
+
+        [TestMethod]
+        public void TestKeep10of5()
+        {
+            var cr = new CandiateRanking[][] { new CandiateRanking[] {
+                new CandiateRanking(0, 10),
+                new CandiateRanking(1, 9),
+                new CandiateRanking(2, 8),
+                new CandiateRanking(3, 7),
+                new CandiateRanking(4, 6)
+            }};
+            var es = new ESKeepBestN(10);
+            var r = es.RunStep(new Person[] { new Person(0, 1, 2, 3) }, cr);
+            Assert.AreEqual(5, r.Length, "# of kept candidates");
+        }
+    }
+}
