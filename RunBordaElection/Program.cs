8c893ea02f43bad8b3bfe2878cd26646f69f25e2
Gordon Watts <gordon@gordon@gordonandpaula.net>
2012-06-04 22:07:22 -0700

    Added a borda election runner. :-)

---
+++ RunBordaElection/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectionDriver;
+
+namespace RunBordaElection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint nElections = 1000;
+            uint nCandidates = 4;
+
+            var e = new Election()
+            {
+                NumberOfCandidates = (int)nCandidates,
+                NumberOfPeople = 400
+            };
+            e.AddStep(new ESBorda());
+
+            var flips = e.RunElectionEnsemble(nElections).Result;
+
+            Console.WriteLine("Saw {0} flips in {1} elections.", flips.flips, nElections);
+            for (int icand = 0; icand < flips.candidateResults.Length; icand++)
+            {
+                Console.Write("Candidate {0}: ", icand);
+                for (int irank = 0; irank < flips.candidateResults.Length; irank++)
+                {
+                    Console.Write("{0}={1}, ", irank, flips.candidateResults[icand].resultTimes[irank]);
+                }
+                Console.WriteLine();
+            }
+
+            // Do a trend as a function of candidate 0...
+
+            var eTrend = new ElectionTrend(e);
+            var results = eTrend.RunTrend(
+                (point, numPoints) => Tuple.Create<double, Func<Person, bool>>(0.1 * point, p => p.Ranking(0) == nCandidates - 1),
+                points: 10
+                );
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                var r = results[i].Result;
+                Console.WriteLine("Election with candidate 0 having {0}% of the vote ({1} flips):", 10.0 * i, r.flips);
+                for (int icand = 0; icand < flips.candidateResults.Length; icand++)
+                {
+                    Console.Write("  Candidate {0}: ", icand);
+                    for (int irank = 0; irank < r.candidateResults.Length; irank++)
+                    {
+                        Console.Write("{0}={1}, ", irank, r.candidateResults[icand].resultTimes[irank]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
